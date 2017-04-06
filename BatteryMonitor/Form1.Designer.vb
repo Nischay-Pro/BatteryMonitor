c1747c6a5c4fd19f770e9fada63153a520840c77
Nischay Pro <NischayPro@gmail.com>
2017-04-06 19:52:51 +0530

    Initial Commit

---
+++ BatteryMonitor/Form1.Designer.vb
@@ -0,0 +1,223 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.CheckBox3 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.NumericUpDown1 = New System.Windows.Forms.NumericUpDown()
+        Me.NumericUpDown2 = New System.Windows.Forms.NumericUpDown()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.NotifyIcon1 = New System.Windows.Forms.NotifyIcon(Me.components)
+        Me.Timer3 = New System.Windows.Forms.Timer(Me.components)
+        Me.GroupBox1.SuspendLayout()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumericUpDown2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(32, 24)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Label1"
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(32, 49)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(39, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Label2"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Button2)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.NumericUpDown2)
+        Me.GroupBox1.Controls.Add(Me.NumericUpDown1)
+        Me.GroupBox1.Controls.Add(Me.CheckBox3)
+        Me.GroupBox1.Controls.Add(Me.CheckBox2)
+        Me.GroupBox1.Controls.Add(Me.CheckBox1)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 92)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(546, 114)
+        Me.GroupBox1.TabIndex = 2
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Configuration and Notifications"
+        '
+        'CheckBox3
+        '
+        Me.CheckBox3.AutoSize = True
+        Me.CheckBox3.Location = New System.Drawing.Point(238, 35)
+        Me.CheckBox3.Name = "CheckBox3"
+        Me.CheckBox3.Size = New System.Drawing.Size(239, 17)
+        Me.CheckBox3.TabIndex = 2
+        Me.CheckBox3.Text = "Enable Warning when battery is fully charged"
+        Me.CheckBox3.UseVisualStyleBackColor = True
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.Location = New System.Drawing.Point(23, 58)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(417, 17)
+        Me.CheckBox2.TabIndex = 1
+        Me.CheckBox2.Text = "Enable Warning when battery drops below or gets charged above certain threshold"
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(23, 35)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(163, 17)
+        Me.CheckBox1.TabIndex = 0
+        Me.CheckBox1.Text = "Run on Startup (Runs in tray)"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(483, 212)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "Hide to Tray"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'NumericUpDown1
+        '
+        Me.NumericUpDown1.Enabled = False
+        Me.NumericUpDown1.Location = New System.Drawing.Point(80, 81)
+        Me.NumericUpDown1.Name = "NumericUpDown1"
+        Me.NumericUpDown1.Size = New System.Drawing.Size(101, 20)
+        Me.NumericUpDown1.TabIndex = 4
+        Me.NumericUpDown1.Value = New Decimal(New Integer() {25, 0, 0, 0})
+        '
+        'NumericUpDown2
+        '
+        Me.NumericUpDown2.Enabled = False
+        Me.NumericUpDown2.Location = New System.Drawing.Point(288, 81)
+        Me.NumericUpDown2.Name = "NumericUpDown2"
+        Me.NumericUpDown2.Size = New System.Drawing.Size(101, 20)
+        Me.NumericUpDown2.TabIndex = 5
+        Me.NumericUpDown2.Value = New Decimal(New Integer() {80, 0, 0, 0})
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(32, 83)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(27, 13)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Min:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(246, 83)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(30, 13)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Max:"
+        '
+        'Button2
+        '
+        Me.Button2.Enabled = False
+        Me.Button2.Location = New System.Drawing.Point(442, 77)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(98, 25)
+        Me.Button2.TabIndex = 7
+        Me.Button2.Text = "Save"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Timer2
+        '
+        '
+        'NotifyIcon1
+        '
+        Me.NotifyIcon1.Icon = CType(resources.GetObject("NotifyIcon1.Icon"), System.Drawing.Icon)
+        Me.NotifyIcon1.Text = "Battery Monitor"
+        Me.NotifyIcon1.Visible = True
+        '
+        'Timer3
+        '
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(570, 247)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Battery Monitor"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NumericUpDown2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Label2 As Label
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents CheckBox3 As CheckBox
+    Friend WithEvents CheckBox2 As CheckBox
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents NumericUpDown2 As NumericUpDown
+    Friend WithEvents NumericUpDown1 As NumericUpDown
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Timer2 As Timer
+    Friend WithEvents NotifyIcon1 As NotifyIcon
+    Friend WithEvents Timer3 As Timer
+End Class
---
+++ BatteryMonitor/Form1.vb
@@ -0,0 +1,143 @@
+﻿Imports BatteryMonitor.IniFile
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        LoadSettings()
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Dim power As PowerStatus = SystemInformation.PowerStatus
+        Dim percent As Single = power.BatteryLifePercent
+        Label1.Text = "Percent battery life remaining: " & percent * 100
+        Dim iSpan As TimeSpan = TimeSpan.FromSeconds(power.BatteryLifeRemaining)
+        Label2.Text = "Battery Life Remaining: " & iSpan.Hours.ToString.PadLeft(2, "0"c) & ":" &
+                        iSpan.Minutes.ToString.PadLeft(2, "0"c) & ":" &
+                        iSpan.Seconds.ToString.PadLeft(2, "0"c)
+    End Sub
+    Private Function LoadSettings()
+        Dim ini As New IniFile
+        Try
+            ini.Load(My.Application.Info.DirectoryPath & "\settings.ini")
+            If ini.GetKeyValue("General", "Startup") = "True" Then
+                CheckBox1.Checked = True
+            End If
+            If ini.GetKeyValue("General", "Threshold") = "True" Then
+                CheckBox2.Checked = True
+                NumericUpDown1.Enabled = True
+                NumericUpDown2.Enabled = True
+                NumericUpDown1.Value = ini.GetKeyValue("General", "Min")
+                NumericUpDown2.Value = ini.GetKeyValue("General", "Max")
+                Timer2.Start()
+            End If
+            If ini.GetKeyValue("General", "Overcharge") = "True" Then
+                CheckBox3.Checked = True
+            End If
+            Return True
+        Catch ex As Exception
+            Return False
+        End Try
+    End Function
+
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+        Dim ini As New IniFile
+        Try
+            ini.Load(My.Application.Info.DirectoryPath & "\settings.ini")
+        Catch ex As Exception
+        End Try
+        ini.SetKeyValue("General", "Startup", CheckBox1.Checked)
+        ini.Save(My.Application.Info.DirectoryPath & "\settings.ini")
+    End Sub
+
+    Private Sub CheckBox2_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox2.CheckedChanged
+        Dim ini As New IniFile
+        Try
+            ini.Load(My.Application.Info.DirectoryPath & "\settings.ini")
+        Catch ex As Exception
+        End Try
+        ini.SetKeyValue("General", "Threshold", CheckBox2.Checked)
+        ini.SetKeyValue("General", "Min", NumericUpDown1.Value)
+        ini.SetKeyValue("General", "Max", NumericUpDown2.Value)
+        ini.Save(My.Application.Info.DirectoryPath & "\settings.ini")
+        NumericUpDown1.Enabled = CheckBox2.Checked
+        NumericUpDown2.Enabled = CheckBox2.Checked
+        If CheckBox2.Checked = False Then
+            Timer2.Stop()
+        Else
+            Timer2.Start()
+        End If
+    End Sub
+
+    Private Sub NumericUpDown1_ValueChanged(sender As Object, e As EventArgs) Handles NumericUpDown1.ValueChanged
+        Button2.Enabled = True
+    End Sub
+
+    Private Sub NumericUpDown2_ValueChanged(sender As Object, e As EventArgs) Handles NumericUpDown2.ValueChanged
+        Button2.Enabled = True
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If NumericUpDown1.Value >= NumericUpDown2.Value Then
+            MsgBox("Error Minimum value can't be greater than or equal to Maximum value", MsgBoxStyle.Information, "Error")
+            NumericUpDown1.Value = NumericUpDown2.Value - 1
+            Exit Sub
+        End If
+        If NumericUpDown2.Value <= NumericUpDown1.Value Then
+            MsgBox("Error Maximum value can't be lesser than or equal to Minimum value", MsgBoxStyle.Information, "Error")
+            NumericUpDown2.Value = NumericUpDown1.Value + 1
+            Exit Sub
+        End If
+        Dim ini As New IniFile
+        Try
+            ini.Load(My.Application.Info.DirectoryPath & "\settings.ini")
+        Catch ex As Exception
+        End Try
+        ini.SetKeyValue("General", "Threshold", CheckBox2.Checked)
+        ini.SetKeyValue("General", "Min", NumericUpDown1.Value)
+        ini.SetKeyValue("General", "Max", NumericUpDown2.Value)
+        ini.Save(My.Application.Info.DirectoryPath & "\settings.ini")
+        NumericUpDown1.Enabled = CheckBox2.Checked
+        NumericUpDown2.Enabled = CheckBox2.Checked
+        Button2.Enabled = False
+    End Sub
+
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+        If NumericUpDown1.Enabled = NumericUpDown2.Enabled = True Then
+            Dim power As PowerStatus = SystemInformation.PowerStatus
+            Dim percent As Single = power.BatteryLifePercent
+            If percent * 100 <= NumericUpDown1.Value Then
+                NotifyIcon1.BalloonTipText = "Battery Level is below threshold of " & NumericUpDown1.Value & ". We suggest you connect your laptop for charging."
+                NotifyIcon1.BalloonTipTitle = "Battery Monitor"
+                NotifyIcon1.BalloonTipIcon = ToolTipIcon.Warning
+                NotifyIcon1.ShowBalloonTip(5000)
+                Timer3.Start()
+                Timer2.Stop()
+            End If
+            If percent * 100 >= NumericUpDown2.Value Then
+                NotifyIcon1.BalloonTipText = "Battery Level is above threshold of " & NumericUpDown2.Value & ". We suggest you disconnect your laptop from charging."
+                NotifyIcon1.BalloonTipTitle = "Battery Monitor"
+                NotifyIcon1.BalloonTipIcon = ToolTipIcon.Warning
+                NotifyIcon1.ShowBalloonTip(5000)
+                Timer3.Start()
+                Timer2.Stop()
+            End If
+        End If
+    End Sub
+
+    Private Sub Timer3_Tick(sender As Object, e As EventArgs) Handles Timer3.Tick
+        Dim power As PowerStatus = SystemInformation.PowerStatus
+        Dim percent As Single = power.BatteryLifePercent * 100
+        If power.BatteryChargeStatus = BatteryChargeStatus.Charging And percent > NumericUpDown1.Value And percent < NumericUpDown2.Value Then
+            Timer2.Start()
+            Timer3.Stop()
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Me.Hide()
+        Me.ShowInTaskbar = False
+    End Sub
+
+    Private Sub NotifyIcon1_MouseDoubleClick(sender As Object, e As MouseEventArgs) Handles NotifyIcon1.MouseDoubleClick
+        Me.Show()
+        Me.ShowInTaskbar = True
+    End Sub
+End Class
